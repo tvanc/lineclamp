72a2228afafbea261092bc223cd74a73c7923438
Travis Van Couvering <travis@turibe.com>
2019-10-17 19:30:21 -0700

    Add lenient-mode test

--- test/test.js
+++ test/test.js
@@ -15,7 +15,26 @@ describe('LineClamp', () => {
     assert.equal(
       clamp.currentLineHeight,
       element.clientHeight,
-      ''
+      'Element reduced to one strict line.'
+    );
+  });
+
+  it('Lenient', () => {
+    const element = document.getElementById('lenientTester');
+    const clamp = new LineClamp(element, {
+      maxLines: 1,
+      strict:   false,
+    });
+
+    clamp.clamp();
+
+    const currentLineHeight = clamp.currentLineHeight;
+    const currentHeight = element.clientHeight;
+
+    assert.isTrue(
+      element.clientHeight > currentLineHeight
+      && currentHeight % currentLineHeight === 0,
+      'One line in original font size.'
     );
   });
 });
