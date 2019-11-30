aa46a1ef214840705637fb63557f32fa73b574d1
Travis Van Couvering <travis@tvanc.com>
2019-11-30 13:19:39 -0800

    Publish minified JS to npm

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,20 @@
+import { terser } from 'rollup-plugin-terser'
+
 const rollupConfig = {
   input: 'src/LineClamp.js',
   output: [
-    {
-      file: 'dist/esm.js',
-      format: 'esm',
-    },
-    {
-      file: 'dist/index.js',
-      format: 'cjs',
-    },
-    {
-      file: 'dist/umd.js',
-      format: 'umd',
-      name: 'LineClamp'
-    },
+    { format: 'esm', file: 'dist/esm.js' },
+    { format: 'esm', file: 'dist/esm.min.js' },
+
+    { format: 'cjs', file: 'dist/index.js' },
+    { format: 'cjs', file: 'dist/index.min.js' },
+
+    { format: 'umd', file: 'dist/umd.js', name: 'LineClamp' },
+    { format: 'umd', file: 'dist/umd.min.js', name: 'LineClamp' },
   ],
+  plugins: [terser({
+    include: /.+\.min\.js$/i
+  })],
 }
 
 export default rollupConfig
