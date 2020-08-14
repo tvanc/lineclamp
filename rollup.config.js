2897874bb69db6d46e68cb7fa06179d2f9a1e40b
Travis Van Couvering <travis@tvanc.com>
2020-08-13 23:02:39 -0700

    Bring rollup.config.js up to date with dep updates

--- rollup.config.js
+++ rollup.config.js
@@ -4,17 +4,14 @@ const rollupConfig = {
   input: 'src/LineClamp.js',
   output: [
     { format: 'esm', file: 'dist/esm.js' },
-    { format: 'esm', file: 'dist/esm.min.js' },
+    { format: 'esm', file: 'dist/esm.min.js', plugins: [terser()] },
 
-    { format: 'cjs', file: 'dist/index.js' },
-    { format: 'cjs', file: 'dist/index.min.js' },
+    { format: 'cjs', file: 'dist/index.js', exports: "default" },
+    { format: 'cjs', file: 'dist/index.min.js', plugins: [terser()], exports: 'default' },
 
     { format: 'umd', file: 'dist/umd.js', name: 'LineClamp' },
-    { format: 'umd', file: 'dist/umd.min.js', name: 'LineClamp' },
-  ],
-  plugins: [terser({
-    include: /.+\.min\.js$/i
-  })],
+    { format: 'umd', file: 'dist/umd.min.js', name: 'LineClamp', plugins: [terser()] },
+  ]
 }
 
 export default rollupConfig
